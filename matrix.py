6ff89b7baa67aac39f2e6059d6246cd812bf46d8
Ryan Yoshida <ryan.yoshida@comcast.net>
2021-01-18 15:20:34 -0800

    added some basic operations to matrix.py

---
+++ matrix.py
@@ -0,0 +1,112 @@
+
+class Matrix:
+    def __init__(self, base, height):
+        self.N = height
+        self.M = base
+
+        self.matrix = [[0 for _ in range(base)] for __ in range(height)]
+
+    def __mul__(self, other):
+        assert(type(other) == Matrix)
+        assert(self.N == other.M), f'{self}.height != {other}.base'
+        assert(self.M == other.N), f'{self}.base != {other}.height'
+
+        product = Matrix(self.N, other.M)
+        
+        for i in range(other.M):
+            for j in range(self.N):
+                for k in range(self.M):
+                    product.matrix[j][i] += self.matrix[j][k] * other.matrix[k][i]
+
+        return product
+
+    def _pow_helper(self, k):
+        if k == 0:
+            return Matrix.get_identity(self.N)
+        elif k == 1:
+            return self
+        else:
+            m = self._pow_helper(k//2)
+            if k%2:
+                return m*m*Matrix.get_identity(self.N)
+            else:
+                return m*m
+
+    def __add__(self, other):
+        assert(type(other) == Matrix)
+        assert(self.N == other.N)
+        assert(self.M == other.M)
+
+        m = Matrix(self.N, self.M)
+        for i in range(self.N):
+            for j in range(self.M):
+                m.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
+
+        return m
+
+    def __sub__(self, other):
+        pass
+
+
+    def __pow__(self, k):
+        assert(type(k) == int)
+        assert(self.N == self.M)
+        return self._pow_helper(k)
+        
+    def __repr(self):
+        return f'Matrix({self.M},{self.N})'
+
+    def __str__(self):
+        return 'Matrix:\n' + '\n'.join(str(row) for row in self.matrix)
+    
+    def get_transpose(self):
+        assert(self.N == self.M)
+        t = Matrix(self.N, self.N)
+        for i in range(self.N):
+            for j in range(self.N):
+                t.matrix[i][j] = self.matrix[j][i]
+
+        return t
+
+
+    @classmethod
+    def get_identity(cls, base):
+        identity = Matrix(base, base)
+        for i in range(base):
+            identity.matrix[i][i] = 1
+        return identity
+
+
+m1 = Matrix(2,3)
+m2 = Matrix(3,2)
+
+m1.matrix = [
+    [1,2],
+    [0,3],
+    [2,1],
+]
+
+m2.matrix = [
+    [2,1,1],
+    [1,3,0],
+]
+
+print(m1*m2)
+
+m3 = Matrix(2,2)
+m3.matrix = [
+    [1, 1],
+    [1, 0],
+]
+
+print(m3**10)
+
+m4 = Matrix(2,2)
+m4.matrix = [
+    [1, 2],
+    [4, 3],
+]
+print(m4.get_transpose())
+
+print(m3 + m4)
+
