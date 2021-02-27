57fc8cd0bab7150e096289e4c9b0660d77a1ff79
Ryan Yoshida <ryan.yoshida@comcast.net>
2021-02-26 21:29:10 -0800

    GNU PBDS

---
+++ gnu_pds.cpp
@@ -0,0 +1,16 @@
+// 5 lines
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class K, class V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+// Initialize with:
+// 	ordered_set<T> os;
+//
+// Functionality
+// 	os.find_by_order()
+// 	os.order_of_key()
