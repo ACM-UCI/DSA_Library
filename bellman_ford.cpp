9926add473b1159465d9db82edd277f32cf9cd07
Ryan Yoshida <33506695+ryoshida28@users.noreply.github.com>
2021-02-27 17:33:46 -0800

    Update bellman_ford.cpp
    changed INFINITY to INF

--- bellman_ford.cpp
+++ bellman_ford.cpp
@@ -2,8 +2,8 @@
 
 // ========== FOR THE USER TO DEFINE ========== //
 typedef int WeightType;					// by default all edge weight computations will use integers
-const WeightType INFINITY = 1e9;		// by default a path of infinite cost will be 1e9
-const WeightType NEG_INFINITY = -1e9;	// by default a negative weight of infinite value is -1e9
+const WeightType INF= 1e9;		// by default a path of infinite cost will be 1e9
+const WeightType NEG_INF = -1e9;	// by default a negative weight of infinite value is -1e9
 // ============================================ //
 
 
@@ -21,30 +21,30 @@ typedef std::vector<WeightType> Costs;	// the return type of the bellmanFord() f
 // END GRAPH ABSTRACTION ======================
 
 
-// From a single source returns a Costs vector containing the cost of the shortest path to all nodes. If unreachable will be INFINITY. 
-// If reachable through negative cycle will be NEG_INFINITY.
+// From a single source returns a Costs vector containing the cost of the shortest path to all nodes. If unreachable will be INF. 
+// If reachable through negative cycle will be NEG_INF.
 Costs bellmanFord(AdjList& adj, int source) {
 	
-	Costs dists(adj.size(), INFINITY);
+	Costs dists(adj.size(), INF);
 	dists[source] = 0;	// SSSP to source is 0
 	for (int i = 0; i < adj.size()-1; ++i) {
 		for (int u = 0; u < adj.size(); ++u) {
 			for (int j = 0; j < adj[u].size(); ++j) {
 				Edge& e = adj[u][j];
-				if (dists[u] != INFINITY) 
+				if (dists[u] != INF) 
 					dists[e.v] = std::min(dists[e.v], dists[u] + e.w);
 			}
 		}
 	}
 
 	// Check for Negative Cycles
-	for (int i = 0; i < adj.size(); ++i) {	// This outer loop is required to determine which nodes have a -INFINITY path
+	for (int i = 0; i < adj.size(); ++i) {	// This outer loop is required to determine which nodes have a -INF path
 											// You can remove this loop if you only wish to find the existence of negative cycles, in which case just check for one dists[i]==NEG_INFINITY
 		for (int u = 0; u < adj.size(); ++u) {
 			for (int j = 0; j < adj[u].size(); ++j) {
 				Edge& e = adj[u][j];
-				if (dists[u] != INFINITY && dists[e.v] > dists[u] + e.w) {
-					dists[e.v] = NEG_INFINITY;
+				if (dists[u] != INF && dists[e.v] > dists[u] + e.w) {
+					dists[e.v] = NEG_INF;
 				}
 			}
 		}
